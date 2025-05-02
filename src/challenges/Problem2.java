5b04e3a0c0b95978acd313e87243e3c46d2e4743
ananyag11 <151110222+ananyag11@users.noreply.github.com>
2025-05-01 23:09:56 -0400

    Create Problem2.java

---
+++ src/challenges/Problem2.java
@@ -0,0 +1,11 @@
+
+package challenges.problem2;
+
+import datastructures.BinaryTreeNode;
+
+public class Problem2 {
+    public static int maxGamblingProfit(BinaryTreeNode root) {
+        return 0;
+    }
+
+}
